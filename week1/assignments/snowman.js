d3d608b6a335a6ad464da05a5bf4ab998bc8bf7a
missamyjo <aflattum@mcad.edu>
2022-01-23 20:53:13 -0600

    edits

--- week1/assignments/snowman.js
+++ week1/assignments/snowman.js
@@ -1,38 +1,41 @@
 let grid = undefined;
-let stroke1 = prompt("enter a basic color name in lower case", "gray");
-let stroke2 = prompt("enter another basic color lower case", "purple");
+let stroke1 = prompt("in lowercase, enter a basic color", "black");
+let stroke2 = prompt("pick another basic color", "red");
+
 function setup() {
     createCanvas(1000, 800);
-    background("#ccc");
+    background("lightblue");
     grid = loadImage("img/100px_grid.png");
 }
+
 function draw() {
-    // background(grid);
+    //  background(grid);
     //snowman legs
-    fill("#f1f1f1");
-    strokeWeight(20);
-    stroke(stroke1);
+    fill("#f1f1f1")
+    stroke(stroke1)
+    strokeWeight(20)
     //left leg
-    ellipse(350, 650, 200);
-    // right leg
-    ellipse(650, 650, 200);
+    ellipse(350, 650, 200)
+    //right leg
+    ellipse(650, 650, 200)
     // body
-    ellipse(500, 450, 350, 400);
+    ellipse(500, 450, 400, 400)
     // head
-    ellipse(500, 200, 200);
+    ellipse(500, 200, 200)
     // hat brim
-    stroke(stroke2);
-    strokeWeight(40);
-    line(400, 120, 600, 120);
+    stroke(stroke2)
+    strokeWeight(34)
+    line(400, 130, 600, 130);
     // hat body
-    quad(400, 50, 600, 50, 550, 120, 450, 120);
+    fill(stroke2)
+    triangle(500, 40, 540, 100, 460, 100);
     // eyes
-    stroke(0);
-    strokeWeight(50);
-    point(425, 200);
-    point(575, 200);
-    // mouth
-    noFill();
-    strokeWeight(10);
-    arc(500, 200, 80, 80, 0, HALF_PI);
+    stroke(0)
+    strokeWeight(50)
+    point(450, 180)
+    point(550, 180)
+    // smile
+    noFill()
+    strokeWeight(10)
+    arc(500, 220, 120, 100, 0, PI)
 }

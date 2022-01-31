43f0b37bf00f0f9e9910db4fbdeec1df2e0192b0
missamyjo <aflattum@mcad.edu>
2022-01-30 21:12:20 -0600

    forgot Scooby Doo

--- week2/assignment/objects.js
+++ week2/assignment/objects.js
@@ -3,6 +3,7 @@ const myTrivia = [
     { question: 'Who is the most famous pug', answer: 'Doug the Pug' },
     { question: 'Who is the dog from Peanuts?', answer: 'Snoopy' },
     { question: 'What is the name of the dog on Adventure Time?', answer: 'Jake' },
+    { question: 'What dog solves mysteries with his gang?', answer: 'Scooby Doo' },
 ];
 console.log(myTrivia);
 const randomQuestion = Math.floor(Math.random() * myTrivia.length);

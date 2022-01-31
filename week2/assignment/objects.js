a8047820918a1f0eb7e93a94c162330126f9da48
missamyjo <aflattum@mcad.edu>
2022-01-30 18:30:53 -0600

    Dog Quiz

---
+++ week2/assignment/objects.js
@@ -0,0 +1,11 @@
+const myTrivia = [
+    { question: 'What is the name of the Simpsons dog?', answer: 'Santas Little Helper' },
+    { question: 'Who is the most famous pug', answer: 'Doug the Pug' },
+    { question: 'Who is the dog from Peanuts?', answer: 'Snoopy' },
+    { question: 'What is the name of the dog on Adventure Time?', answer: 'Jake' },
+];
+console.log(myTrivia);
+const randomQuestion = Math.floor(Math.random() * myTrivia.length);
+const answer = window.prompt(myTrivia[randomQuestion].question);
+window.alert('You answered ' + answer + '. The correct answer was ' + myTrivia[randomQuestion].answer + '.');
+

248611bd686987884488f15b9e6b1468f7ebc4cd
missamyjo <aflattum@mcad.edu>
2022-02-19 16:10:49 -0600

    bubbles

--- week5/classes/classes.js
+++ week5/classes/classes.js
@@ -1,32 +1,37 @@
 let bubble1;
 let bubble2;
+let bubble3;
 
 function setup() {
-    createCanvas(600, 400);
-    bubble1 = new Bubble();
-    bubble2 = new Bubble();
+    createCanvas(windowWidth, windowHeight);
+    bubble1 = new Bubble(200, 200, 40);
+    bubble2 = new Bubble(400, 200, 20);
+    bubble3 = new Bubble(100, 300, 20);
 }
 function draw() {
-    background(0);
+    background('purple');
     bubble1.move();
     bubble1.show();
     bubble2.move();
     bubble2.show();
+    bubble3.move();
+    bubble3.show();
 }
 
 class Bubble {
-    constructor() {
-        this.x = 200;
-        this.y = 150;
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
     }
     move() {
-        this.x = this.x + random(-5, 5);
-        this.y = this.y + random(-5, 5);
+        this.x = this.x + random(1, -1);
+        this.y = this.y + random(2, 0);
     }
     show() {
-        stroke(255);
-        strokeWeight(4);
+        stroke('lightblue');
+        strokeWeight(2);
         noFill();
-        ellipse(this.x, this.y, 24, 24);
+        ellipse(this.x, this.y, this.r * 2);
     }
 }

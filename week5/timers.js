cce3d8746fccad11b6a94c6c15012577995e5148
missamyjo <aflattum@mcad.edu>
2022-02-19 08:59:44 -0600

    timers

---
+++ week5/timers.js
@@ -0,0 +1,40 @@
+let blockX = 0;
+let blockY = 0;
+let blockColor = 255;
+let drawTimer;
+const speed = 10;
+const distance = 2;
+function setup() {
+    createCanvas(500, 500,);
+    background(0);
+}
+
+function drawBlock(x, y, color) {
+    fill(color || 255);
+    rect(x, y, 50, 50);
+}
+function keyTyped() {
+    let KeyToNumber = Number(key);
+    if (isNaN(keytoNumber)) {
+        return;
+    }
+    keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+    console.log('converted number', keyToNumber);
+    blockColor = keyToNumber;
+}
+window.setTimeout(() => {
+    drawTimer = window.setInterval(() => {
+        if (blockY - 50 <= height) {
+            drawBlock(blockX, blockY, 255);
+            blockY += distance;
+        } else {
+            blockY = 0;
+            blockX += 50;
+        }
+        if (blockY - 50 > height && blockX - 50 > width) {
+            window.clearInterval(drawTimer);
+            alert('done');
+        }
+
+    }, speed);
+}, 1500);

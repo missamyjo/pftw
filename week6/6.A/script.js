e1fd8f927a7cfbc768bf5da63e1b70ab5cdaa347
missamyjo <aflattum@mcad.edu>
2022-02-26 16:17:42 -0600

    llamas

---
+++ week6/6.A/script.js
@@ -0,0 +1,20 @@
+
+var llamaFacts = new Vue({
+    el: '#llama-facts',
+    data: {
+        header: "10 Things About Llamas!",
+        parentMessage: 'Llamas',
+        items: [
+            { message: 'are vegetarians.' },
+            { message: 'have a lifespan of 20 years.' },
+            { message: 'are social and prefer to live in a herd.' },
+            { message: 'can grow up to 6 feet tall.' },
+            { message: 'can hum.' },
+            { message: 'are used as therapy animals.' },
+            { message: 'weigh between 280 and 450 pounds.' },
+            { message: 'can shoot green spit up to 10 feet away.' },
+            { message: 'are classified according to their fur length: Ccara are  short coated and Cucara are medium coated.' },
+            { message: 'have two wild "cousins": the vicuña and the guanaco.' },
+        ]
+    }
+})

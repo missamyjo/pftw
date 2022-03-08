66c3a5b222bf8b5a0278b779c82151a404ba2e52
missamyjo <aflattum@mcad.edu>
2022-03-07 18:35:10 -0600

    7.A

---
+++ week7/7.A/script.js
@@ -0,0 +1,72 @@
+Vue.createApp({
+    data() {
+        return {
+            pez: [
+                {
+                    pezName: "Wonder Woman",
+                    pezFace: "images/wonderwoman.jpg",
+                    backingCard: "Yes",
+                    madeIn: "Slovenia",
+                    pezYear: "1985",
+                    pezQuantity: "3",
+                },
+                {
+                    pezName: "Marge Simpson",
+                    pezFace: "images/marge.jpg",
+                    backingCard: "Yes",
+                    madeIn: "Hungary",
+                    pezYear: "unknown",
+                    pezQuantity: "1",
+                },
+                {
+                    pezName: "Darth Vader",
+                    pezFace: "images/darth.jpg",
+                    backingCard: "Yes",
+                    madeIn: "China",
+                    pezYear: "1997",
+                    pezQuantity: "2",
+                },
+                {
+                    pezName: "C-3PO",
+                    pezFace: "images/c3po.jpg",
+                    backingCard: "Yes",
+                    madeIn: "Hungary",
+                    pezYear: "1997",
+                    pezQuantity: "1",
+                },
+                {
+                    pezName: "Princess Leia",
+                    pezFace: "images/leia.jpg",
+                    backingCard: "No",
+                    madeIn: "Hungary",
+                    pezYear: "1997",
+                    pezQuantity: "1",
+                },
+                {
+                    pezName: "Yoda",
+                    pezFace: "images/yoda.jpg",
+                    backingCard: "No",
+                    madeIn: "Slovenia",
+                    pezYear: "1997",
+                    pezQuantity: "1",
+                },
+                {
+                    pezName: "Garfield",
+                    pezFace: "images/garfield.jpg",
+                    backingCard: "No",
+                    madeIn: "China",
+                    pezYear: "unknown",
+                    pezQuantity: "1",
+                },
+                {
+                    pezName: "Nermal",
+                    pezFace: "images/nermal.jpg",
+                    backingCard: "No",
+                    madeIn: "Slovenia",
+                    pezYear: "unknown",
+                    pezQuantity: "1",
+                },
+            ]
+        }
+    }
+}).mount("#app");

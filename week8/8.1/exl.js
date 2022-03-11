c6e5ef4db0ab28827f3819a62c18a1b3676b8ce0
missamyjo <aflattum@mcad.edu>
2022-03-11 12:51:28 -0600

    8.1

--- week8/8.1/exl.js
+++ week8/8.1/exl.js
@@ -6,69 +6,70 @@ Vue.createApp({
             street: '',
             city: '',
             state: 'MN',
-            availableStates: [{
-                value: 'AL', name: 'Alabama'
-                value: 'AK', name: 'Alaska'
-                value: 'AZ', name: 'Arizona'
-                value: 'AR', name: 'Arkansas'
-                value: 'CA', name: 'California'
-                value: 'CO', name: 'Colorado'
-                value: 'CT', name: 'Connecticut'
-                value: 'DE', name: 'Delaware'
-                value: 'FL', name: 'Florida'
-                value: 'GA', name: 'Georgia'
-                value: 'HI', name: 'Hawaii'
-                value: 'ID', name: 'Idaho'
-                value: 'IL', name: 'Illinois'
-                value: 'IN', name: 'Indiana'
-                value: 'IA', name: 'Iowa'
-                value: 'KS', name: 'Kansas'
-                value: 'KY', name: 'Kentucky'
-                value: 'LA', name: 'Louisiana'
-                value: 'ME', name: 'Maine'
-                value: 'MD', name: 'Maryland'
-                value: 'MA', name: 'Massachusetts'
-                value: 'MI', name: 'Michigan'
-                value: 'MN', name: 'Minnesota'
-                value: 'MS', name: 'Mississippi'
-                value: 'MO', name: 'Missouri'
-                value: 'MT', name: 'Montana'
-                value: 'NE', name: 'Nebraska'
-                value: 'NV', name: 'Nevada'
-                value: 'NH', name: 'New Hampshire'
-                value: 'NJ', name: 'New Jersey'
-                value: 'NM', name: 'New Mexico'
-                value: 'NY', name: 'New York'
-                value: 'NC', name: 'North Carolina'
-                value: 'ND', name: 'North Dakota'
-                value: 'OH', name: 'Ohio'
-                value: 'OK', name: 'Oklahoma'
-                value: 'OR', name: 'Oregon'
-                value: 'PA', name: 'Pennsylvania'
-                value: 'RI', name: 'Rhode Island'
-                value: 'SC', name: 'South Carolina'
-                value: 'SD', name: 'South Dakota'
-                value: 'TN', name: 'Tennessee'
-                value: 'TX', name: 'Texas'
-                value: 'UT', name: 'Utah'
-                value: 'VT', name: 'Vermont'
-                value: 'VA', name: 'Virginia'
-                value: 'WA', name: 'Washington'
-                value: 'WV', name: 'West Virginia'
-                value: 'WI', name: 'Wisconsin'
-                value: 'WY', name: 'Wyoming'
-            }],
+            availableStates: [
+                { value: 'AL', name: 'Alabama' },
+                { value: 'AK', name: 'Alaska' },
+                { value: 'AR', name: 'Arkansas' },
+                { value: 'AZ', name: 'Arizona' },
+                { value: 'CA', name: 'California' },
+                { value: 'CO', name: 'Colorado' },
+                { value: 'CT', name: 'Connecticut ' },
+                { value: 'DE', name: 'Deleware' },
+                { value: 'DC', name: 'District of Columbia' },
+                { value: 'FL', name: 'Florida' },
+                { value: 'GA', name: 'Georgia' },
+                { value: 'HI', name: 'Hawaii' },
+                { value: 'ID', name: 'Idaho' },
+                { value: 'IL', name: 'Illinois' },
+                { value: 'IN', name: 'Indiana' },
+                { value: 'IA', name: 'Iowa' },
+                { value: 'KA', name: 'Kansas' },
+                { value: 'KY', name: 'Kentucky' },
+                { value: 'LA', name: 'Louisiana' },
+                { value: 'ME', name: 'Maine' },
+                { value: 'MA', name: 'Massachusets ' },
+                { value: 'MD', name: 'Maryland' },
+                { value: 'MI', name: 'Michigan' },
+                { value: 'MN', name: 'Minnesota' },
+                { value: 'MS', name: 'Mississippi' },
+                { value: 'MO', name: 'Missouri' },
+                { value: 'MT', name: 'Montana' },
+                { value: 'NE', name: 'Nebraska' },
+                { value: 'NH', name: 'New Hampshire' },
+                { value: 'NM', name: 'New Mexico' },
+                { value: 'NJ', name: 'New Jersey' },
+                { value: 'NY', name: 'New York' },
+                { value: 'NV', name: 'Nevada' },
+                { value: 'NC', name: 'North Carolina' },
+                { value: 'ND', name: 'North Dakota' },
+                { value: 'OH', name: 'Ohio' },
+                { value: 'OK', name: 'Oklahoma' },
+                { value: 'OR', name: 'Oregon' },
+                { value: 'PA', name: 'Pennsylvania' },
+                { value: 'PR', name: 'Puerto Rico' },
+                { value: 'RI', name: 'Rhode Island' },
+                { value: 'SD', name: 'South Dakota' },
+                { value: 'SC', name: 'South Carolina' },
+                { value: 'TN', name: 'Tennessee ' },
+                { value: 'TX', name: 'Texas' },
+                { value: 'UT', name: 'Utah' },
+                { value: 'VT', name: 'Vermont' },
+                { value: 'VA', name: 'Virginia' },
+                { value: 'WA', name: 'Washington' },
+                { value: 'WV', name: 'West Virginia' },
+                { value: 'WI', name: 'Wisconsin' },
+                { value: 'WY', name: 'Wyoming' },
+            ],
             selectedColors: [],
-            colors: [{
-                name: 'White', value: 'white',
-                name: 'Beige', value: 'beige',
-                name: 'Fawn (light, medium, dark)', value: 'fawn',
-                name: 'Brown (light, medium, dark)', value: 'brown',
-                name: 'Bat Black', value: 'bay-black',
-                name: 'True Black', value: 'true-black',
-                name: 'Rose Grey (light, medium, dark)', value: 'rose-grey',
-                name: 'Silver Grey (light, medium, dark)', value: 'silver-grey'
-            }
+            colors: [
+                { name: 'White', value: 'white' },
+                { name: 'Beige', value: 'beige' },
+                { name: 'Fawn (light, medium, dark)', value: 'fawn' },
+                { name: 'Brown (light, medium, dark)', value: 'brown' },
+                { name: 'Bay Black', value: 'bay-black' },
+                { name: 'True Black', value: 'true-black' },
+                { name: 'Rose Gray (light, medium, dark)', value: 'rose-gray' },
+                { name: 'Silver Gray (light, medium, dark)', value: 'silver-gray' },
             ],
             status: ''
         }

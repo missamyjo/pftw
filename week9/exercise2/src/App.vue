0aaea1818c96842fe2037c5b4843d13e7b3b59f1
missamyjo <aflattum@mcad.edu>
2022-03-28 15:38:58 -0500

    9.2

---
+++ week9/exercise2/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import CamelidRow from "../src/components/CamelidRow.vue";
+// javascript
+const camelids = [
+  {
+    commonName: "Lama",
+    binomialName: "Lama glama",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Lama",
+    species: "L. glama",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+  },
+  {
+    commonName: "Alpaca",
+    binomialName: "Vicugna pacos",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Vicugna",
+    species: "V. pacos",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+  },
+];
+</script>
+
+<template>
+  <!-- html -->
+  <div>
+    <h1>This will be my Camelids page</h1>
+    <div class="camelid-container">
+      <CamelidRow
+        v-for="critter in camelids"
+        v-bind:key="critter.commonName"
+        v-bind:animal="critter"
+      />
+    </div>
+  </div>
+  <!--  commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg" -->
+</template>
+
+<style>
+/* css */
+.camelid-container {
+  display: flex;
+  direction: row;
+}
+</style>
---
+++ week9/exercise2/src/components/CamelidRow.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps(["animal"]);
+console.log(props.animal);
+</script>
+<template>
+  <div>
+    <h3>{{ props.animal.commonName }}</h3>
+    <p>Binomial Name: {{ props.animal.binomialName }}</p>
+    <p>Family: {{ props.animal.family }}</p>
+
+    <p>Genus: {{ props.animal.genus }}</p>
+    <p>Species: {{ props.animal.species }}</p>
+    <img v-bind:src="props.animal.image" v-bind:alt="props.animal.commonName" />
+  </div>
+</template>;
+<style scoped>
+div {
+  max-width: 220px;
+  border: 1px solid;
+  border-radius: 4px;
+  padding: 10px;
+  margin: 10px;
+}
+img {
+  max-width: 200px;
+  display: block;
+}
+h3 {
+  color: red;
+}
+</style>
